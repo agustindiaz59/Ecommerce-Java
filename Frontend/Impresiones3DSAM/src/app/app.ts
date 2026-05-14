b328019a523306e8d830cfe6c7289d489de1d11c
agustindiaz59 <matutias10000@gmail.com>
2026-05-14 18:17:54 -0300

    Encabezado de la página

--- Frontend/Impresiones3DSAM/src/app/app.ts
+++ Frontend/Impresiones3DSAM/src/app/app.ts
@@ -1,13 +1,20 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
+import { Carousel } from "./components/carousel/carousel";
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar],
+  imports: [Navbar, Carousel],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit{
   protected readonly title = signal('Impresiones3DSAM');
+
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
